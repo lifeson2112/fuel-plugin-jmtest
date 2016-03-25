ffc76f5660139bf2ef8101a729a3fec08db71461
root <jmiller@gmail.com>
2016-03-25 10:18:40 -0400

    write file

--- deployment_scripts/puppet/modules/jmtest/manifests/write-root-file.pp
+++ deployment_scripts/puppet/modules/jmtest/manifests/write-root-file.pp
@@ -1,8 +1,11 @@
+notice("MODULAR: write-root-file.pp")
 # this is 'jmtest' key is defined by 'name' in metadata.yaml
 # all actual data is defined in environment_config.yaml 
-$jmtest_data = hiera('jmtest')
+$jmtest_data = hiera('jmtest',{})
 
 $file_name = $jmtest_data['root_filename']
 $file_content = $jmtest_data['jmtest_text_to_write']
 
-
+file { "/root/${file_name}":
+  content => $file_content,
+}
